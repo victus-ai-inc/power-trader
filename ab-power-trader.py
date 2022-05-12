530b6f1df1f1e8c56e265d93be21e15b934b8175
ryan-bulger <ryan.d.bulger@gmail.com>
2022-05-12 16:06:22 -0600

    adding dates

--- ab-power-trader.py
+++ ab-power-trader.py
@@ -5,14 +5,18 @@ import pandas as pd
 import numpy as np
 import altair as alt
 from st_aggrid import AgGrid
-#import NRGstream script
+import datetime
+#import NRGstream py script 
+#https://stackoverflow.com/questions/1186789/what-is-the-best-way-to-call-a-script-from-another-script
 
-# Configuring app setup
+# App config
 st.set_page_config(layout='wide', initial_sidebar_state='auto', menu_items=None)
 st.title('Alberta Power Trader')
 
-st.sidebar.write('test')
+# Sidebar config
+start_date = st.sidebar.date_input('Start Date', value=datetime.datetime.now()-datetime.timedelta(30))
+end_date = st.sidebar.date_input('End Date', min_value=start_date)
 
 if __name__ == '__main__':
-    #call NRGstream def
+    #call NRGstream py script
     pass
