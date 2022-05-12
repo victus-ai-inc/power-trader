cb29a2d4e92da999aef51c052734994311b758af
ryan-bulger <ryan.d.bulger@gmail.com>
2022-05-12 14:43:46 -0600

    added requirements.txt again

--- ab-power-trader.py
+++ ab-power-trader.py
@@ -5,9 +5,14 @@ import pandas as pd
 import numpy as np
 import altair as alt
 from st_aggrid import AgGrid
+#import NRGstream script
 
 # Configuring app setup
 st.set_page_config(layout='wide', initial_sidebar_state='auto', menu_items=None)
 st.title('Alberta Power Trader')
 
 st.sidebar.write('test')
+
+if __name__ == '__main__':
+    #call NRGstream def
+    pass
