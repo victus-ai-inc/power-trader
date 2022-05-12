5d79937d277691a713a94c22eef1f8fd074aa913
ryan-bulger <ryan.d.bulger@gmail.com>
2022-05-12 14:13:38 -0600

    moved requirements

--- ab-power-trader.py
+++ ab-power-trader.py
@@ -6,3 +6,6 @@ import numpy as np
 import altair as alt
 from st_aggrid import AgGrid
 
+# Configuring app setup
+#st.set_page_config(layout='wide', initial_sidebar_state='auto', menu_items=None)
+st.write('Alberta Power Trader')
