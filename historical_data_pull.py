4c50f6cee07e23e3013aa7b8764aa6ab369d8ef0
ryan-bulger <ryan.d.bulger@gmail.com>
2022-06-14 13:09:06 -0600

    preparing hist for db entries

--- historical_data_pull.py
+++ historical_data_pull.py
@@ -5,11 +5,13 @@ import pull_nrg_data
 
 if __name__ == '__main__':
     tic = time.perf_counter()
-    streamId = 129796
+    
+    streamId = 41371
     year = 2021
     # Get NRG API token
     accessToken, tokenExpiry = pull_nrg_data.getToken()
     print(accessToken)
+    # Cycle through months and write to DB
     for month in range(1,13):
         startDate = date(year,month,1)
         if month < 12:
@@ -17,8 +19,11 @@ if __name__ == '__main__':
         else:
             endDate = date(year,12,31)
         APIdata = pull_nrg_data.pull_data(startDate.strftime('%m/%d/%Y'), endDate.strftime('%m/%d/%Y'), streamId, accessToken, tokenExpiry)
-        print(startDate,endDate)
-        print(APIdata)
+        # Write data to DB
+        
+
+    # Release token
     pull_nrg_data.release_token(accessToken)
+    
     toc = time.perf_counter()
     print(f'{toc - tic:0.2f} secs')
