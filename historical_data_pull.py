0c0223c1e247e2ac669ed34c15131b917ce8898c
ryan-bulger <ryan.d.bulger@gmail.com>
2022-06-22 15:44:38 -0600

    testing sql

--- historical_data_pull.py
+++ historical_data_pull.py
@@ -2,6 +2,20 @@ from datetime import datetime, timedelta, date
 import time
 import pandas as pd
 import pull_nrg_data
+import mysql.connector
+from mysql.connector import errorcode
+
+def sql_insert():
+    nrgdata = mysql.connector.connect(user='victusai', password='#p!k4XG2Mg#,B,W', host='localhost', database='nrgdata')
+    print(nrgdata)
+    mycursor = nrgdata.cursor()
+    #mycursor.execute("CREATE TABLE customers (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), address VARCHAR(255))")
+    sql = "INSERT INTO customers (name, address) VALUES (%s, %s)"
+    val = ("John", "Highway 21")
+    mycursor.execute(sql, val)
+    nrgdata.commit()
+    print(mycursor.rowcount, "record inserted.")
+    nrgdata.close()
 
 if __name__ == '__main__':
     tic = time.perf_counter()
@@ -12,15 +26,16 @@ if __name__ == '__main__':
     accessToken, tokenExpiry = pull_nrg_data.getToken()
     print(accessToken)
     # Cycle through months and write to DB
-    for month in range(1,13):
+    for month in range(1,2):
         startDate = date(year,month,1)
         if month < 12:
             endDate = date(year,month+1,1)
         else:
-            endDate = date(year,12,31)
+            endDate = date(year+1,1,1)
         APIdata = pull_nrg_data.pull_data(startDate.strftime('%m/%d/%Y'), endDate.strftime('%m/%d/%Y'), streamId, accessToken, tokenExpiry)
-        # Write data to DB
-        
+    print(type(APIdata))
+    # Write data to DB
+    APIdata.to_csv('test.csv', index=False)
 
     # Release token
     pull_nrg_data.release_token(accessToken)
