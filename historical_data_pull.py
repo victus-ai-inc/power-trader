5ed82ba887d188835ad84e72070614ce08c63620
ryan-bulger <ryan.d.bulger@gmail.com>
2022-06-23 14:27:07 -0600

    first multi stream/year push to sql

--- historical_data_pull.py
+++ historical_data_pull.py
@@ -17,24 +17,26 @@ def sql_insert(df):
 if __name__ == '__main__':
     tic = time.perf_counter()
     
-    streamId = 41371
-    year = 2020
+    streamId = [129796, 45132]
+    year = [2020, 2021]
     # Get NRG API token
     accessToken, tokenExpiry = pull_nrg_data.getToken()
     print(accessToken)
     # Cycle through months and write to DB
-    for month in range(1,2):
-        startDate = date(year,month,1)
-        if month < 12:
-            endDate = date(year,month+1,1)
-        else:
-            endDate = date(year+1,1,1)
-        APIdata = pull_nrg_data.pull_data(startDate.strftime('%m/%d/%Y'), endDate.strftime('%m/%d/%Y'), streamId, accessToken, tokenExpiry)
-        APIdata['timeStamp'] = pd.to_datetime(APIdata['timeStamp'])
-        # Write data to DB
-        sql_insert(APIdata)
-
-    APIdata.to_csv('test.csv', index=False)
+    for id in streamId:
+        for yr in year:
+            for month in range(1,2):
+                if datetime.now() >= tokenExpiry:
+                    accessToken, tokenExpiry = pull_nrg_data.getToken()
+                startDate = date(yr,month,1)
+                if month < 12:
+                    endDate = date(yr,month+1,1)
+                else:
+                    endDate = date(yr+1,1,1)
+                APIdata = pull_nrg_data.pull_data(startDate.strftime('%m/%d/%Y'), endDate.strftime('%m/%d/%Y'), id, accessToken, tokenExpiry)
+                APIdata['timeStamp'] = pd.to_datetime(APIdata['timeStamp'])
+                # Write data to DB
+                sql_insert(APIdata)
 
     # Release token
     pull_nrg_data.release_token(accessToken)
