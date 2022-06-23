e95e66ff91c34a080a0e50801cd4094040698307
ryan-bulger <ryan.d.bulger@gmail.com>
2022-06-22 18:58:40 -0600

    sql export working now

--- historical_data_pull.py
+++ historical_data_pull.py
@@ -7,14 +7,6 @@ from mysql.connector import errorcode
 
 def sql_insert():
     nrgdata = mysql.connector.connect(user='victusai', password='#p!k4XG2Mg#,B,W', host='localhost', database='nrgdata')
-    print(nrgdata)
-    mycursor = nrgdata.cursor()
-    #mycursor.execute("CREATE TABLE customers (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), address VARCHAR(255))")
-    sql = "INSERT INTO customers (name, address) VALUES (%s, %s)"
-    val = ("John", "Highway 21")
-    mycursor.execute(sql, val)
-    nrgdata.commit()
-    print(mycursor.rowcount, "record inserted.")
     nrgdata.close()
 
 if __name__ == '__main__':
@@ -33,7 +25,8 @@ if __name__ == '__main__':
         else:
             endDate = date(year+1,1,1)
         APIdata = pull_nrg_data.pull_data(startDate.strftime('%m/%d/%Y'), endDate.strftime('%m/%d/%Y'), streamId, accessToken, tokenExpiry)
-    print(type(APIdata))
+    APIdata['timeStamp'] = pd.to_datetime(APIdata['timeStamp'])
+
     # Write data to DB
     APIdata.to_csv('test.csv', index=False)
 
--- pull_nrg_data.py
+++ pull_nrg_data.py
@@ -5,7 +5,6 @@ import certifi
 import ssl
 import json
 import ab_power_trader
-import streamlit as st
 
 # Pull NRG credentials from Streamlit secrets manager
 username, password = ab_power_trader.get_nrg_creds()
@@ -39,7 +38,7 @@ def getToken():
 
 def get_streamInfo(streamId):
     streamInfo = pd.read_csv('stream_codes.csv')
-    streamInfo = streamInfo[streamInfo['streamId']==str(streamId)]
+    streamInfo = streamInfo[streamInfo['streamId']==streamId]
     return streamInfo
 
 # Use token to pull data
@@ -55,29 +54,26 @@ def pull_data(fromDate, toDate, streamId, accessToken, tokenExpiry):
     conn.request('GET', path, None, headers)
     res = conn.getresponse()
     res_code = res.status
-    try:
-        #Load json data & create pandas df
-        jsonData = json.loads(res.read().decode('utf-8'))
-        df = pd.json_normalize(jsonData, record_path='data')
-        #Rename df cols
-        df.rename(columns={0:'timeStamp', 1:'value'}, inplace=True)
-        #Add streamInfo cols to df
-        streamInfo = get_streamInfo(streamId)
-        assetCode = streamInfo.iloc[0,1]
-        streamName = streamInfo.iloc[0,2]
-        fuelType = streamInfo.iloc[0,3]
-        subfuelType = streamInfo.iloc[0,4]
-        timeInterval = streamInfo.iloc[0,5]
-        intervalType = streamInfo.iloc[0,6]
-        df = df.assign(streamId=streamId, assetCode=assetCode, streamName=streamName, fuelType=fuelType, \
-                        subfuelType=subfuelType, timeInterval=timeInterval, intervalType=intervalType)
-        #Changing 'value' col to numeric and filling in NA's with previous value in col
-        df['value'] = pd.to_numeric(df['value'])
-        df.fillna(method='ffill',inplace=True)
-        st.write(df)
-        conn.close()
-    except:
-        print('Exception Caught 1')
+
+    # Load json data & create pandas df
+    jsonData = json.loads(res.read().decode('utf-8'))
+    df = pd.json_normalize(jsonData, record_path='data')
+    # Rename df cols
+    df.rename(columns={0:'timeStamp', 1:'value'}, inplace=True)
+    # Add streamInfo cols to df
+    streamInfo = get_streamInfo(streamId)
+    assetCode = streamInfo.iloc[0,1]
+    streamName = streamInfo.iloc[0,2]
+    fuelType = streamInfo.iloc[0,3]
+    subfuelType = streamInfo.iloc[0,4]
+    timeInterval = streamInfo.iloc[0,5]
+    intervalType = streamInfo.iloc[0,6]
+    df = df.assign(streamId=streamId, assetCode=assetCode, streamName=streamName, fuelType=fuelType, \
+                    subfuelType=subfuelType, timeInterval=timeInterval, intervalType=intervalType)
+    # Changing 'value' col to numeric and filling in NA's with previous value in col
+    df['value'] = pd.to_numeric(df['value'])
+    df.fillna(method='ffill',inplace=True)
+    conn.close()
     return df
 
 # Release generated token
@@ -88,7 +84,6 @@ def release_token(accessToken):
     conn = http.client.HTTPSConnection(server,context=context)
     conn.request('DELETE', path, None, headers)
     res = conn.getresponse()
-    st.write('Token successfully released.')
     print('token released')
 
 # accessToken = 'fP6LEG8qlowwkH-fHeToGJyHgGk7-zR3VTfbACuvnDFeEPssPLa-H4-3BCEv36dhmQJMhaL9n1-cBtRUOFIpe0QFHexes63zOBCngY6iH-4SwFGsMT0llA0Tdr-ckOTVs_1GYI0Es9UCXX-Higx8iXIMX-rSSyMQcjoivSifZm6PO3Y-RbrHvcHpRBwCZzwCpYblHuGVW8_t1nuKbT_H7eb0C8-eqWU4HKtNLE_Ukslbn54rR3E2AKcRw9ZVSiwf-mheeTb-t--fG1Vb5z6r6RLUDei6it3qQfmphK3F7uPHNgf_JtCI6mpBc07_yi_UuHqNYE2Fg8LTpcuaB34WuLg1eMw'
