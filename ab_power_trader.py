683db4db21250fe3bbbcf4ac6046037f11bfcca1
Ryan Bulger <r.bulger@victusai.com>
2022-07-07 16:04:26 -0600

    hiding menus

--- ab_power_trader.py
+++ ab_power_trader.py
@@ -15,6 +15,7 @@ def hide_menu(bool):
         hide_menu_style = """
             <style>
             #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
             </style>
             """
         return st.markdown(hide_menu_style, unsafe_allow_html=True)
