0c6893f825bbe78a70dab8a0b4456207323fdb58
Ryan Bulger <r.bulger@victusai.com>
2022-07-27 13:07:21 -0600

    changes

--- data_pull.py
+++ data_pull.py
@@ -6,31 +6,27 @@ from google.cloud import bigquery
 from google.oauth2 import service_account
 from google.cloud.exceptions import NotFound
 
-# Google BigQuery auth
-credentials = service_account.Credentials.from_service_account_info(st.secrets["gcp_service_account"])
-
-def get_streams():
-    streams = pd.read_csv('stream_codes.csv')
-    # Pull only 5-min supply streams
-    lst = [int(id) for id in streams[(streams['timeInterval']=='5 min') & (streams['intervalType']=='supply')]['streamId']]
-    print(f'Streams = {lst}')
-    return lst
-
 if __name__ == '__main__':
-    streamIds = get_streams()
-    year = [2022]
+# Google BigQuery auth
+    credentials = service_account.Credentials.from_service_account_info(st.secrets["gcp_service_account"])
+# Pull stream data
+    streams = pd.read_csv('stream_codes.csv')
+    streamIds = [129795]
+    #streamIds = [int(id) for id in streams[(streams['timeInterval']=='1 hr') & (streams['intervalType']=='supply')]['streamId']]
+    year = [2020]
     stream_count = len(streamIds)
     count = 0
     for id in streamIds:
         for yr in year:
             startDate = date(yr,1,1)
-            endDate = date(yr,7,1)
+            endDate = date(yr+1,1,1)
             accessToken, tokenExpiry = pull_nrg_data.getToken()
             try:
                 APIdata = pull_nrg_data.pull_data(startDate.strftime('%m/%d/%Y'), endDate.strftime('%m/%d/%Y'), id, accessToken, tokenExpiry)
                 pull_nrg_data.release_token(accessToken)
                 APIdata['timeStamp'] = pd.to_datetime(APIdata['timeStamp'])
-                bigquery.Client(credentials=credentials).load_table_from_dataframe(APIdata, 'nrgdata.nrgdata')
+                print(APIdata)
+                bigquery.Client(credentials=credentials).load_table_from_dataframe(APIdata, 'nrgdata.hourly_data')
             except:
                 pull_nrg_data.release_token(accessToken)
                 pass
