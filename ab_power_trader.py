00f8593e02965c88ffce16debdf5bebb3c6634bc
Ryan Bulger <r.bulger@victusai.com>
2022-07-28 11:08:50 -0600

    moving charts below live data pull

--- ab_power_trader.py
+++ ab_power_trader.py
@@ -15,7 +15,7 @@ from google.cloud.exceptions import NotFound
 from pandasql import sqldf
 import time
 
-def current_data():
+def current_data() -> pd.DataFrame:
     streamIds = [86, 322684, 322677, 87, 85, 23695, 322665, 23694]
     current_df = pd.DataFrame([])
     today = datetime.now()
@@ -36,7 +36,7 @@ def current_data():
             strftime('%m', timeStamp) AS month,
             strftime('%d', timeStamp) AS day,
             strftime('%H', timeStamp) AS hour,
-            avg(value)
+            avg(value) AS value
         FROM current_df
         GROUP BY fuelType, year, month, day, hour
         '''
@@ -103,6 +103,39 @@ def pull_grouped_hist():
     #history_df['date'] = pd.to_datetime(history_df[['year','month','day']])
     return history_df
 
+# Grid options for AgGrid Demand forecast table
+grid_options = {
+    "defaultColDef":
+    {
+        "autoHeight": True,
+        "editable": False,
+        "sortable": False,
+        "filterable": False,
+        "suppressMovable": True,
+        "resizable":False,
+    },
+    "RowHoverHighlight":True,
+    "columnDefs": [
+        {
+            "headerName": "Date",
+            "field": "Date",
+            "type":"dateColumn",
+        },
+        {
+            "headerName": "Demand (MW)",
+            "field": "Close",
+            "valueFormatter": "Math.floor(data.Close).toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')",
+        },
+        {
+            "headerName": "Offset (MW)",
+            "field": "Offset",
+            "editable": True,
+            "singleClickEdit":True,
+            "valueFormatter": "Math.floor(data.Offset).toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')",
+        },
+    ],
+}
+
 # Main code block
 if __name__ == '__main__':
 
@@ -112,100 +145,59 @@ if __name__ == '__main__':
     hide_menu(True)
 
 # Pull 24M Supply/Demand data from AESO
+    st.subheader('24M Supply/Demand data from AESO (Daily)')
     streamIds = [278763]
     streamNames = {278763:'0'}
     years = [datetime.now().year, datetime.now().year+1, datetime.now().year+2]
     offset_df = stream_data(streamIds, streamNames, years)
     offset_df.rename(columns={'0':'Peak Hour', 2:'Expected Supply', 3:'Import Capacity', 4:'Load+Reserve', 5:'Surplus'}, inplace=True)
     st.write(offset_df)
-    st.write(pull_grouped_hist())
 
-# Pull live data
-    st.write(current_data())
-    for seconds in range(20):
-        placeholder = st.empty()
-        with placeholder.container():
-            st.dataframe(current_data())
-            time.sleep(10)
-
-# Grid options for AgGrid Demand forecast table
-    grid_options = {
-        "defaultColDef":
-        {
-            "autoHeight": True,
-            "editable": False,
-            "sortable": False,
-            "filterable": False,
-            "suppressMovable": True,
-            "resizable":False,
-        },
-        "RowHoverHighlight":True,
-        "columnDefs": [
-            {
-                "headerName": "Date",
-                "field": "Date",
-                "type":"dateColumn",
-            },
-            {
-                "headerName": "Demand (MW)",
-                "field": "Close",
-                "valueFormatter": "Math.floor(data.Close).toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')",
-            },
-            {
-                "headerName": "Offset (MW)",
-                "field": "Offset",
-                "editable": True,
-                "singleClickEdit":True,
-                "valueFormatter": "Math.floor(data.Offset).toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')",
-            },
-        ],
-    }
-
-# Offset demand table & chart
-    st.subheader('Forecasted & Adjusted Demand')
-    # Creating cols for Streamlit app
-    col1, col2 = st.columns([1,2])
-    # Adding AgGrid table to Col1
-    with col1:
-        offset_df = AgGrid(offset_df[['Date','Open','Close','Offset']], grid_options, fit_columns_on_grid_load=True)['data']
-    # Calculating cummulative offset
-        offset_df = pd.DataFrame(offset_df)
-        offset_df['tot_offset'] = offset_df['Offset'].cumsum()
-        offset_df['Adjusted Demand'] = offset_df['Close'] + offset_df['tot_offset']
-        offset_df = offset_df[1:]
-    # Creating Altair charts
-        # Demand candlestick chart
-        open_close_color = alt.condition("datum.Open <= datum.Close",
-                                 alt.value("#06982d"),
-                                 alt.value("#ae1325"))
-        candlestick = alt.Chart(offset_df).mark_bar().encode(
-            x=alt.X('Date:T',axis=alt.Axis(format='%b %Y'),title=''),
-            y=alt.Y('Open:Q', title='Demand (MW)'),
-            y2=alt.Y2('Close:Q', title=''),
-            color=open_close_color
-        ).interactive( ).properties(height=430)
-        # Adjusted Demand area chart
-        area = alt.Chart(offset_df).mark_area(color='grey', opacity=0.3).encode(
-            x='Date:T',
-            y='Adjusted Demand:Q',
-        ).interactive(bind_y=True)
-        # Selector for sliding vertical line
-        nearest = alt.selection(type='single', nearest=True, on='mouseover', fields=['Date'], empty='none', clear='mouseout')
-        # Creating vertical line to highlight current Date
-        rule = alt.Chart(offset_df).mark_rule(color='gray').encode(
-            x='Date:T',
-            opacity=alt.condition(nearest, alt.value(1), alt.value(0)),
-            tooltip = ['Date:T', 
-                        alt.Tooltip('Open:Q', title='Open (MW)', format=',d'),
-                        alt.Tooltip('Close:Q', title='Close (MW)', format=',d'),
-                        alt.Tooltip('Adjusted Demand:Q', title='Adjusted Close (MW)', format=',d')]
-        ).add_selection(nearest)
-    # Adding layered chart to Col2
-    with col2:
-        st.altair_chart(candlestick + area + rule, use_container_width=True)
+# # Offset demand table & chart
+#     st.subheader('Forecasted & Adjusted Demand')
+#     # Creating cols for Streamlit app
+#     col1, col2 = st.columns([1,2])
+#     # Adding AgGrid table to Col1
+#     with col1:
+#         offset_df = AgGrid(offset_df[['Date','Open','Close','Offset']], grid_options, fit_columns_on_grid_load=True)['data']
+#     # Calculating cummulative offset
+#         offset_df = pd.DataFrame(offset_df)
+#         offset_df['tot_offset'] = offset_df['Offset'].cumsum()
+#         offset_df['Adjusted Demand'] = offset_df['Close'] + offset_df['tot_offset']
+#         offset_df = offset_df[1:]
+#     # Creating Altair charts
+#         # Demand candlestick chart
+#         open_close_color = alt.condition("datum.Open <= datum.Close",
+#                                  alt.value("#06982d"),
+#                                  alt.value("#ae1325"))
+#         candlestick = alt.Chart(offset_df).mark_bar().encode(
+#             x=alt.X('Date:T',axis=alt.Axis(format='%b %Y'),title=''),
+#             y=alt.Y('Open:Q', title='Demand (MW)'),
+#             y2=alt.Y2('Close:Q', title=''),
+#             color=open_close_color
+#         ).interactive( ).properties(height=430)
+#         # Adjusted Demand area chart
+#         area = alt.Chart(offset_df).mark_area(color='grey', opacity=0.3).encode(
+#             x='Date:T',
+#             y='Adjusted Demand:Q',
+#         ).interactive(bind_y=True)
+#         # Selector for sliding vertical line
+#         nearest = alt.selection(type='single', nearest=True, on='mouseover', fields=['Date'], empty='none', clear='mouseout')
+#         # Creating vertical line to highlight current Date
+#         rule = alt.Chart(offset_df).mark_rule(color='gray').encode(
+#             x='Date:T',
+#             opacity=alt.condition(nearest, alt.value(1), alt.value(0)),
+#             tooltip = ['Date:T', 
+#                         alt.Tooltip('Open:Q', title='Open (MW)', format=',d'),
+#                         alt.Tooltip('Close:Q', title='Close (MW)', format=',d'),
+#                         alt.Tooltip('Adjusted Demand:Q', title='Adjusted Close (MW)', format=',d')]
+#         ).add_selection(nearest)
+#     # Adding layered chart to Col2
+#     with col2:
+#         st.altair_chart(candlestick + area + rule, use_container_width=True)
 
 # Outages chart
-    st.subheader('Forecasted Outages')
+    st.subheader('Forecasted Outages (Daily)')
     #Create outages_df
     streamIds = [44648, 118361, 322689, 118362, 147262, 322675, 322682, 44651]
     streamNames = {44648:'Coal', 118361:'Gas', 322689:'Dual Fuel', 118362:'Hydro', 147262:'Wind', 322675:'Solar', 322682:'Energy Storage', 44651:'Biomass & Other'}
@@ -233,12 +225,31 @@ if __name__ == '__main__':
         color=alt.Color('Source:N', scale=alt.Scale(scheme='category20'))
     ).transform_filter(selection).properties(width=1300)
     st.altair_chart(outage_area & outage_bar, use_container_width=True)
+    
+    placeholder = st.empty()
+    for seconds in range(100000):
+        current_df = current_data()
+        with placeholder.container():
+            # Pull live data
+            st.subheader('Live Data (hourly)')
+            st.dataframe(current_df)
+            chart = alt.Chart(current_df).mark_area().encode(
+                x='hour',
+                y='value:Q',
+                color='fuelType:N',
+                tooltip=['fuelType:N','hour','value:Q']
+            ).properties(width=1000).interactive()
+            st.altair_chart(chart)
 
-# Pull historical data
-    history_df = pull_grouped_hist()
-    hist = alt.Chart(history_df).mark_area().encode(
-        x='date:T',
-        y=alt.Y('Close:Q', stack='zero'),
-        color='subfuelType'
-    ).interactive()
-    st.altair_chart(hist, use_container_width=True)
+            # Pull historical data
+            st.subheader('Historical data from Google BigQuery (hourly)')
+            history_df = pull_grouped_hist()
+            st.write(history_df)
+            hist = alt.Chart(history_df).mark_area().encode(
+                    x='hour',
+                    y='value:Q',
+                    color='fuelType:N'
+                ).interactive()
+            st.altair_chart(hist, use_container_width=True)
+
+            time.sleep(5)
