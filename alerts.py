19b14f7da9527f545d691a010bb78d2282e5cd4e
Ryan Bulger <r.bulger@victusai.com>
2022-08-08 14:31:34 -0600

    sms messaging func working

--- alerts.py
+++ alerts.py
@@ -1,18 +1,17 @@
 import streamlit as st
 import smtplib
+import json
 from twilio.rest import Client
-import smtplib 
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
+from datetime import datetime
 
 def whatsapp():
     # Your Account SID from twilio.com/console
     account_sid = "AC480f9d79462ace32036b1915d38601d6"
     # Your Auth Token from twilio.com/console
     auth_token  = "ef0f023c38dc9ee13e523c6bce388caa"
-
     client = Client(account_sid, auth_token)
-
     message = client.messages.create(
         to="+14035129991", 
         from_="+18573845191",
@@ -20,38 +19,18 @@ def whatsapp():
     print(message.sid)
 
 def sms():
-    email = "Your Email"
-    pas = "Your Pass"
-
-    sms_gateway = 'number@tmomail.net'
-    # The server we use to send emails in our case it will be gmail but every email provider has a different smtp 
-    # and port is also provided by the email provider.
-    smtp = "smtp.gmail.com" 
+    email = st.secrets['email_address']
+    pas = st.secrets['email_password']
+    sms_gateways = st.secrets['phone_numbers'].values()
+    smtp = 'smtp.gmail.com'
     port = 587
-    # This will start our email server
-    server = smtplib.SMTP(smtp,port)
-    # Starting the server
+    server = smtplib.SMTP(smtp, port)
     server.starttls()
-    # Now we need to login
-    server.login(email,pas)
-
-    # Now we use the MIME module to structure our message.
+    server.login(email, pas)
     msg = MIMEMultipart()
-    msg['From'] = email
-    msg['To'] = sms_gateway
-    # Make sure you add a new line in the subject
-    msg['Subject'] = "You can insert anything\n"
-    # Make sure you also add new lines to your body
-    body = "You can insert message here\n"
-    # and then attach that body furthermore you can also send html content.
-    msg.attach(MIMEText(body, 'plain'))
-
-    sms = msg.as_string()
-
-    server.sendmail(email,sms_gateway,sms)
-
-# email = st.secrets['gmail_address']
-# pas = st.secrets['gmail_password']
-
-if __name__ == '__main__':
-    pass
+    for gw in sms_gateways:
+        msg['To'] = gw
+        body = f'NEW ALERT:\n{datetime.now().strftime("%a %b %d, %Y @ %-I:%M%p")}\nhttps://bit.ly/3zFIAj8'
+        msg.attach(MIMEText(body, 'plain'))
+        sms = msg.as_string()
+        server.sendmail(email, gw, sms)
---
+++ pull_nrg_data.py
@@ -0,0 +1,87 @@
+from datetime import datetime, timedelta
+import pandas as pd
+import http.client
+import certifi
+import ssl
+import json
+import streamlit as st
+
+# Pull NRG credentials from Streamlit secrets manager
+def get_nrg_creds():
+    username = st.secrets["nrg_username"]
+    password = st.secrets["nrg_password"]
+    return username, password
+
+# Use creds to generate token from NRG
+def getToken():
+    username, password = get_nrg_creds()
+    server = 'api.nrgstream.com'
+    tokenPath = '/api/security/token'
+    tokenPayload = f'grant_type=password&username={username}&password={password}'
+    headers = {"Content-type": "application/x-www-form-urlencoded"}
+    # Connect to API server to get a token
+    context = ssl.create_default_context(cafile=certifi.where())
+    conn = http.client.HTTPSConnection(server,context=context)
+    conn.request('POST', tokenPath, tokenPayload, headers)
+    res = conn.getresponse()
+    res_code = res.status
+    # Check if the response is good
+    if res_code == 200:
+        res_data = res.read()
+        # Decode the token into an object
+        jsonData = json.loads(res_data.decode('utf-8'))
+        accessToken = jsonData['access_token']
+        # Calculate new expiry date
+        tokenExpiry = datetime.now() + timedelta(seconds=jsonData['expires_in'])
+    else:
+        res_data = res.read()
+        # print(res_data.decode('utf-8'))
+    conn.close()
+    return (accessToken, tokenExpiry)
+
+def get_streamInfo(streamId):
+    streamInfo = pd.read_csv('stream_codes.csv')
+    streamInfo = streamInfo[streamInfo['streamId']==streamId]
+    return streamInfo
+
+# Use token to pull data
+def pull_data(fromDate, toDate, streamId, accessToken, tokenExpiry):
+    # Setup the path for data request
+    server = 'api.nrgstream.com'
+    path = f'/api/StreamData/{streamId}?fromDate={fromDate}&toDate={toDate}'
+    headers = {'Accept': 'Application/json', 'Authorization': f'Bearer {accessToken}'}
+    context = ssl.create_default_context(cafile=certifi.where())
+    conn = http.client.HTTPSConnection(server, context=context)
+    conn.request('GET', path, None, headers)
+    res = conn.getresponse()
+    # Load json data & create pandas df
+    jsonData = json.loads(res.read().decode('utf-8'))
+    df = pd.json_normalize(jsonData, record_path='data')
+    # Rename df cols
+    df.rename(columns={0:'timeStamp', 1:'value'}, inplace=True)
+    # Add streamInfo cols to df
+    streamInfo = get_streamInfo(streamId)
+    assetCode = streamInfo.iloc[0,1]
+    streamName = streamInfo.iloc[0,2]
+    fuelType = streamInfo.iloc[0,3]
+    subfuelType = streamInfo.iloc[0,4]
+    timeInterval = streamInfo.iloc[0,5]
+    intervalType = streamInfo.iloc[0,6]
+    df = df.assign(streamId=streamId, assetCode=assetCode, streamName=streamName, fuelType=fuelType, \
+                    subfuelType=subfuelType, timeInterval=timeInterval, intervalType=intervalType)
+    # Changing 'value' col to numeric and filling in NA's with previous value in col
+    df.replace(to_replace={'value':''}, value=0, inplace=True)
+    df['value'] = pd.to_numeric(df['value'])
+    df.fillna(method='ffill', inplace=True)
+    conn.close()
+    return df
+
+# Release generated token
+def release_token(accessToken):
+    path = '/api/ReleaseToken'
+    server = 'api.nrgstream.com'
+    headers = {'Authorization': f'Bearer {accessToken}'}
+    context = ssl.create_default_context(cafile=certifi.where())
+    conn = http.client.HTTPSConnection(server,context=context)
+    conn.request('DELETE', path, None, headers)
+    res = conn.getresponse()
