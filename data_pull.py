c4e333b7094b9be50ceac4033465b4cbf50a1c87
Ryan Bulger <r.bulger@victusai.com>
2022-08-09 11:30:37 -0600

    made data_pull.py recursive

--- data_pull.py
+++ data_pull.py
@@ -6,27 +6,34 @@ from google.cloud import bigquery
 from google.oauth2 import service_account
 from google.cloud.exceptions import NotFound
 
+def resursion(i):
+    try:
+        accessToken, tokenExpiry = pull_nrg_data.getToken()
+        APIdata = pull_nrg_data.pull_data(startDate.strftime('%m/%d/%Y'), endDate.strftime('%m/%d/%Y'), id, accessToken, tokenExpiry)
+        pull_nrg_data.release_token(accessToken)
+        APIdata['timeStamp'] = pd.to_datetime(APIdata['timeStamp'])
+        print(APIdata)
+        bigquery.Client(credentials=credentials).load_table_from_dataframe(APIdata, 'nrgdata.hourly_data')
+    except:
+        pull_nrg_data.release_token(accessToken)
+        print(f'STREAM #{id} failed on iteration #{i}. Trying again')
+        if i < 101:
+            i += 1
+            resursion(i)
+
 if __name__ == '__main__':
 # Google BigQuery auth
     credentials = service_account.Credentials.from_service_account_info(st.secrets["gcp_service_account"])
 # Pull stream data
     streams = pd.read_csv('stream_codes.csv')
     streamIds = [int(id) for id in streams[(streams['timeInterval']=='1 hr') & (streams['intervalType']=='supply')]['streamId']]
-    year = [2022]
+    year = [2020, 2021]
     stream_count = len(streamIds)
     count = 1
     for id in streamIds:
         for yr in year:
-            startDate = date(yr,8,3)
-            endDate = date(yr,8,4)
-            accessToken, tokenExpiry = pull_nrg_data.getToken()
-            try:
-                APIdata = pull_nrg_data.pull_data(startDate.strftime('%m/%d/%Y'), endDate.strftime('%m/%d/%Y'), id, accessToken, tokenExpiry)
-                pull_nrg_data.release_token(accessToken)
-                APIdata['timeStamp'] = pd.to_datetime(APIdata['timeStamp'])
-                bigquery.Client(credentials=credentials).load_table_from_dataframe(APIdata, 'nrgdata.hourly_data')
-            except:
-                pull_nrg_data.release_token(accessToken)
-                pass
+            startDate = date(yr,1,1)
+            endDate = date(yr+1,1,1)
+            resursion(1)
         print(f'STREAM #{id} finished, {stream_count-count} streams remaining')
         count += 1
