a13cf5669afe5f2b3b9211737f1a380e134e89b0
Ryan Bulger <r.bulger@victusai.com>
2022-08-24 09:13:22 -0600

    testing

--- Alberta_Power.py
+++ Alberta_Power.py
@@ -162,6 +162,7 @@ def kpi(left_df, right_df, title):
     kpi_df['absDelta'] = abs(kpi_df['delta'])
     # Formatting numbers 
     kpi_df.iloc[:,1:] = kpi_df.iloc[:,1:].applymap('{:.0f}'.format)
+    kpi_df
     col1, col2, col3, col4, col5, col6, col7, col8, col9, col10, col11 = st.columns(11)
     with col1:
         st.subheader(title)
