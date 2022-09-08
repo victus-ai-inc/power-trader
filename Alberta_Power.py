41b4a7dbc98a7d425816639117cad3c488b96493
Ryan Bulger <r.bulger@victusai.com>
2022-09-07 18:01:22 -0600

    change

--- Alberta_Power.py
+++ Alberta_Power.py
@@ -317,8 +317,8 @@ for seconds in range(60000):
             realtime = realtime_df[['fuelType','value','timeStamp']][realtime_df['timeStamp']==max(realtime_df['timeStamp']-timedelta(minutes=5))]   
         realtime.drop('timeStamp', axis=1, inplace=True)
         realtime = realtime.astype({'fuelType':'object','value':'float64'})
-        previousHour = current_df[['fuelType','value']][current_df['hour']==datetime.now().hour-1]
-        currentHour = current_df[['fuelType','value']][current_df['hour']==datetime.now().hour-0]
+        previousHour = current_df[['fuelType','value']][current_df['hour']==datetime.now().hour-7]
+        currentHour = current_df[['fuelType','value']][current_df['hour']==datetime.now().hour-6]
         
         with st.expander('*Click here to expand/collapse KPIs',expanded=True):
             kpi_df = kpi(previousHour, realtime, 'Real Time')
