3efef8a53cad1da0315bdf8228a15c85a921d790
Ryan Bulger <r.bulger@victusai.com>
2022-09-07 19:05:23 -0600

    b

--- Alberta_Power.py
+++ Alberta_Power.py
@@ -354,6 +354,7 @@ for seconds in range(60000):
     
     # Daily outages
         st.subheader('Daily Outages (90-day forecast)')
+        daily_outage
         st.write(daily_outage['timeStamp'].dtypes)
         daily_outage = daily_outage[daily_outage['timeStamp']<(now+timedelta(days=90))]
         chrt = alt.Chart(daily_outage).mark_area(opacity=0.7).encode(
