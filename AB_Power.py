37236f13ecfba95250f519a054fe02e9cc8332a4
Ryan Bulger <r.bulger@victusai.com>
2022-10-27 03:31:10 -0600

    chngs

--- AB_Power.py
+++ AB_Power.py
@@ -19,93 +19,93 @@ from memory_profiler import profile
 #import tracemalloc
 
 st.set_page_config(layout='wide', initial_sidebar_state='collapsed', menu_items=None)
-tracemalloc.start()
-snapshot1 = tracemalloc.take_snapshot()
+# tracemalloc.start()
+# snapshot1 = tracemalloc.take_snapshot()
 
-@st.experimental_singleton
-def init_tracking_object():
-  tracemalloc.start(10)
+# @st.experimental_singleton
+# def init_tracking_object():
+#   tracemalloc.start(10)
 
-  return {
-    "runs": 0,
-    "tracebacks": {}
-  }
-_TRACES = init_tracking_object()
+#   return {
+#     "runs": 0,
+#     "tracebacks": {}
+#   }
+# _TRACES = init_tracking_object()
 
-def traceback_exclude_filter(patterns, tracebackList):
-    """
-    Returns False if any provided pattern exists in the filename of the traceback,
-    Returns True otherwise.
-    """
-    for t in tracebackList:
-        for p in patterns:
-            if p in t.filename:
-                return False
-        return True
+# def traceback_exclude_filter(patterns, tracebackList):
+#     """
+#     Returns False if any provided pattern exists in the filename of the traceback,
+#     Returns True otherwise.
+#     """
+#     for t in tracebackList:
+#         for p in patterns:
+#             if p in t.filename:
+#                 return False
+#         return True
 
-def traceback_include_filter(patterns, tracebackList):
-    """
-    Returns True if any provided pattern exists in the filename of the traceback,
-    Returns False otherwise.
-    """
-    for t in tracebackList:
-        for p in patterns:
-            if p in t.filename:
-                return True
-    return False
+# def traceback_include_filter(patterns, tracebackList):
+#     """
+#     Returns True if any provided pattern exists in the filename of the traceback,
+#     Returns False otherwise.
+#     """
+#     for t in tracebackList:
+#         for p in patterns:
+#             if p in t.filename:
+#                 return True
+#     return False
 
-def check_for_leaks(diff):
-    """
-    Checks if the same traceback appears consistently after multiple runs.
+# def check_for_leaks(diff):
+#     """
+#     Checks if the same traceback appears consistently after multiple runs.
 
-    diff - The object returned by tracemalloc#snapshot.compare_to
-    """
-    _TRACES["runs"] = _TRACES["runs"] + 1
-    tracebacks = set()
+#     diff - The object returned by tracemalloc#snapshot.compare_to
+#     """
+#     _TRACES["runs"] = _TRACES["runs"] + 1
+#     tracebacks = set()
 
-    for sd in diff:
-        for t in sd.traceback:
-            tracebacks.add(t)
+#     for sd in diff:
+#         for t in sd.traceback:
+#             tracebacks.add(t)
 
-    if "tracebacks" not in _TRACES or len(_TRACES["tracebacks"]) == 0:
-        for t in tracebacks:
-            _TRACES["tracebacks"][t] = 1
-    else:
-        oldTracebacks = _TRACES["tracebacks"].keys()
-        intersection = tracebacks.intersection(oldTracebacks)
-        evictions = set()
-        for t in _TRACES["tracebacks"]:
-            if t not in intersection:
-                evictions.add(t)
-            else:
-                _TRACES["tracebacks"][t] = _TRACES["tracebacks"][t] + 1
+#     if "tracebacks" not in _TRACES or len(_TRACES["tracebacks"]) == 0:
+#         for t in tracebacks:
+#             _TRACES["tracebacks"][t] = 1
+#     else:
+#         oldTracebacks = _TRACES["tracebacks"].keys()
+#         intersection = tracebacks.intersection(oldTracebacks)
+#         evictions = set()
+#         for t in _TRACES["tracebacks"]:
+#             if t not in intersection:
+#                 evictions.add(t)
+#             else:
+#                 _TRACES["tracebacks"][t] = _TRACES["tracebacks"][t] + 1
 
-        for t in evictions:
-            del _TRACES["tracebacks"][t]
+#         for t in evictions:
+#             del _TRACES["tracebacks"][t]
 
-    if _TRACES["runs"] > 1:
-        st.write(f'After {_TRACES["runs"]} runs the following traces were collected.')
-        prettyPrint = {}
-        for t in _TRACES["tracebacks"]:
-            prettyPrint[str(t)] = _TRACES["tracebacks"][t]
-        st.write(json.dumps(prettyPrint, sort_keys=True, indent=4))
+#     if _TRACES["runs"] > 1:
+#         st.write(f'After {_TRACES["runs"]} runs the following traces were collected.')
+#         prettyPrint = {}
+#         for t in _TRACES["tracebacks"]:
+#             prettyPrint[str(t)] = _TRACES["tracebacks"][t]
+#         st.write(json.dumps(prettyPrint, sort_keys=True, indent=4))
 
-def compare_snapshots():
-    """
-    Compares two consecutive snapshots and tracks if the same traceback can be found
-    in the diff. If a traceback consistently appears during runs, it's a good indicator
-    for a memory leak.
-    """
-    snapshot = tracemalloc.take_snapshot()
-    if "snapshot" in _TRACES:
-        diff = snapshot.compare_to(_TRACES["snapshot"], "lineno")
-        diff = [d for d in diff if
-                d.count_diff > 0 and traceback_exclude_filter(["tornado"], d.traceback)
-                and traceback_include_filter(["streamlit"], d.traceback)
-                ]
-        check_for_leaks(diff)
+# def compare_snapshots():
+#     """
+#     Compares two consecutive snapshots and tracks if the same traceback can be found
+#     in the diff. If a traceback consistently appears during runs, it's a good indicator
+#     for a memory leak.
+#     """
+#     snapshot = tracemalloc.take_snapshot()
+#     if "snapshot" in _TRACES:
+#         diff = snapshot.compare_to(_TRACES["snapshot"], "lineno")
+#         diff = [d for d in diff if
+#                 d.count_diff > 0 and traceback_exclude_filter(["tornado"], d.traceback)
+#                 and traceback_include_filter(["streamlit"], d.traceback)
+#                 ]
+#         check_for_leaks(diff)
 
-    _TRACES["snapshot"] = snapshot
+#     _TRACES["snapshot"] = snapshot
 
 # **** Create alert if data-manager is not running ****
     # Give option to open data-manager url and allow it to run in background
